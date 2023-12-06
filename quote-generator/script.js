36860fb3bd4bfb449a0aa7df3c154aa2764c5610
Ryan Partridge <rpartridge101@yahoo.co.uk>
2023-12-06 04:28:13 +0000

    Added page loader

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -3,22 +3,41 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show loading
+const loading = () => {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide loading
+const complete = () => {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
+
+
 // Show new quote
 const newQuote = () => {
+    loading();
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     
     // Check quote length to determine styling
     quote.text.length > 120 ? quoteText.classList.add('long-quote') : quoteText.classList.remove('long-quote');
 
     authorText.textContent = !quote.author ? 'Unknown' : quote.author;
+
+    // Set Quote, Hide loader
     quoteText.textContent = quote.text;
+    complete();
 }
 
 // Get Quotes From API
 const getQuotes = async () => {
+    loading();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
 
     try {
